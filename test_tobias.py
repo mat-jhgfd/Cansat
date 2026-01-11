21364cdaf2c6c1a086cf797aa678f6bc8e2f53b8
Tobias-me <101821520+Tobias-me@users.noreply.github.com>
2026-01-11 19:38:27 +0100

    Comment my code...
    Please check my English 🥺

--- test_tobias.py
+++ test_tobias.py
@@ -4,9 +4,10 @@ import time # for: _get_time_HH_MM_SS_mmm
 from machine import RTC # for: _get_time_HH_MM_SS_mmm
 
 class Logger:
-    MAX_LOG_TYPE_LEN = 7
-    MAX_LINES = 100
-    # a line will look like that
+    MAX_LOG_TYPE_LEN = 7  # (Const) : maximum len of a log type. Example: W-A-R-N-I-N-G --> 7 letters
+    MAX_LINES = 100  # (Const) : maximum lines in a single file. After that, it will create a new file (see _make_new_file & _add_line)
+    
+    # a line will look like that:
     # [00:00:15.750] [INFO]		System Started
     # [00:00:17.750] [ERROR]	System Crashed
     # [00:00:17.750] [WARNING]	System Almost Crashed :)
@@ -18,10 +19,10 @@ class Logger:
         Here will be all the setup things.
         """
 
-        # init
-        self.rtc = RTC()
+        # Init classes
+        self.rtc = RTC()  # Init RTC (for the GPS's time)
                 
-        # Check actual log file number & make dir
+        # Check actual log folder number & make new dir
         logs_files_list = os.listdir("logs_files")
         
         self.log_file_number = 0
@@ -30,20 +31,19 @@ class Logger:
         
         os.mkdir(f"logs_files/logs #{log_folder_number}")
         
-        self.log_file_base_path = f"logs_files/logs #{log_folder_number}/log_"
-        
+        self.log_file_base_path = f"logs_files/logs #{log_folder_number}/log_"  # Define the base path. The final path will be: BASE_PATH + N_log + .txt
+
+        # Make the first log file
         self._make_new_file()
 
-        
-        
-        
+    
     def _make_new_file(self):
         """
         Backend Function, do not use it !
         Make a new log file.
         """
-        # Create the file (and add a section)
         
+        # Create the file (and add a section)
         self.log_file_number += 1
         self.log_file_path = self.log_file_base_path + str(self.log_file_number) + ".txt"
         
@@ -51,9 +51,11 @@ class Logger:
             f.write("=== LOGS STARTED (v0.2) ===\n")
         
         self.file = open(self.log_file_path, "a")
-        self.line_count = 0
         
-    def _get_time_HH_MM_SS_mmm(self, bias_ms: int = 0):
+        self.line_count = 0  # Reset the line count
+
+    
+    def _get_time_HH_MM_SS_mmm(self):
         """
         Backend Function, do not use it!
         Return timestamp like this: HH:MM:SS.mmm
@@ -61,7 +63,6 @@ class Logger:
         """       
 
         # 1. Get the wall-clock time from RTC
-        
         dt = self.rtc.datetime()
         hours, minutes, seconds = dt[4], dt[5], dt[6]
 
@@ -72,8 +73,9 @@ class Logger:
 
         # 3. Return the time with padded zeros
         return f"{hours:02d}:{minutes:02d}:{seconds:02d}.{milliseconds:03d}"
+
     
-    def _add_line(self, line_type:str, details:str, time_bias: int = 0):
+    def _add_line(self, line_type:str, details:str):
         """
         Backend Function to add a line, do not use it !
         Add a line for logging (see format in comment at the start of the class)
@@ -81,9 +83,11 @@ class Logger:
         line_type: example: DEBUG, INFO, ...
         details: see format
         """
-        self.line_count += 1
+        
+        self.line_count += 1  # Update the line count (for this file)
+        
         # Setup of the time
-        actual_time = self._get_time_HH_MM_SS_mmm(time_bias)
+        actual_time = self._get_time_HH_MM_SS_mmm()
         final_string = f"[{actual_time}]  "
         
         # Type part
@@ -94,31 +98,32 @@ class Logger:
         
         # The last thing: the details
         final_string += details.strip()
-        
+
+        # Check if we don't need to make a new file
         if self.line_count >= self.MAX_LINES:
-            self.force_saving()
-            self._make_new_file()
+            self.force_saving()  # Save the last one
+            self._make_new_file()  # and create a new one
         
         # Finaly, we write it
         self.file.write(f"{final_string}\n")
-        """
-        with open(self.log_file_path, "a") as f:
-            f.write(f"{final_string}\n")"""
+
     
     def force_saving(self):
         """
         Force the saving of the logs
         """
         self.file.close()
-        self.file = open(self.log_file_path, "a")
-        
+        self.file = open(self.log_file_path, "a")  # "a" --> write in the file and don't delete the content
+
+    
     def add_info_line(self, text):
         """
         add an info line like this:
         [01:32:55.970]  [INFO]    details here
         """
         self._add_line("INFO", str(text))
-        
+
+    
     def add_error_line(self, text):
         """
         add an error line like this:
@@ -128,7 +133,8 @@ class Logger:
 
 
 # Here a little example
-# Note: this will not be executed if the class is ONLY imported
+# Note: this will not be executed if the class is ONLY imported.
+# It's a stress test for 10 000 lines, and it work :) 
 if __name__ == "__main__":
     logger = Logger()
     
@@ -136,4 +142,5 @@ if __name__ == "__main__":
         logger.add_info_line("some text and " + str(i))
     
     logger.force_saving()
+
 
