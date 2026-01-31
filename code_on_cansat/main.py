cf277d7cfeef37b3242ae55f4ef89f01d79684b6
aqwerty <matico.verde@gmail.com>
2026-01-31 16:33:13 +0100

    Debug changes gps and sensor timing
    now the gps is not strictly needed for the program to work
    also the timing of the sensor is set to 1 second. can be changed

--- code_on_cansat/main.py
+++ code_on_cansat/main.py
@@ -4,7 +4,7 @@ from gps import GPSModule
 from sensors import get_sensor_packet
 from radio import RadioTransmitter
 from logger_init import initialize_logger
-from config import SYNC_INTERVAL
+from config import SYNC_INTERVAL, SENSOR_INTERVAL_SECONDS, DEBUG
 
 counter = 1
 
@@ -12,12 +12,15 @@ counter = 1
 gps_module = GPSModule()
 radio = RadioTransmitter()
 logger = initialize_logger()
+last_sensor_time = time.time()
 
 # GPS initialization
 gps_module.init_gps_neo6m()
-gps_module.search_mode()
-
-print("Normal Mode started")
+if not DEBUG:
+    gps_module.search_mode()
+    print("Normal Mode started")
+else :
+    print("Debug mode activated, gps not prioritised")
 
 while True:
     led.toggle()
@@ -27,9 +30,12 @@ while True:
         radio.transmit(gps_module.get_sync_packet(counter))
         time.sleep(0.05)
     
-    # Transmit sensor data
-    radio.transmit(get_sensor_packet(counter, radio.last_rssi, logger))
-    time.sleep(0.05)
+    # Transmit sensor data ONLY every 1 second
+    current_time = time.time()
+    if current_time - last_sensor_time >= SENSOR_INTERVAL_SECONDS:
+        radio.transmit(get_sensor_packet(counter, radio.last_rssi, logger))
+        time.sleep(0.05)
+        last_sensor_time = current_time
     
     # Transmit GPS data if available
     gps_msg = gps_module.get_gps_packet(counter, logger)
